a319ebe3d00ea52cd1f18a21fd1e0458d45711d1
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-08-28 22:56:05 +0500

    add style in sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,14 @@
+
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(2).toggleClass('active');
+$('#first').on('click', () => {
+    $('div').eq(1).fadeOut(800);
 });
 
-$('.active').setAttribute('id', 'active').removeAttribute('id');
-
-$('div').click(function() {
-    console.log($(this).index());
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeOut(800);
 });
 
-// console.log($('div').eq(2).find('.more'));
-
-// console.log($('.some').closest('.findmef').addClass('wsefwef'));
-
-// console.log($('.more').eq(0).siblings());
-
-$('.findme').fadeOut(1800);
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
