b315a4dabf1f02b1052aa6626e41d73123bf4318
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-09-04 21:38:36 +0500

    modal does not jump at any window or document height

--- dist/script.js
+++ dist/script.js
@@ -1924,12 +1924,16 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
     Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(e => {
       e.preventDefault();
       Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeIn(500);
-      const targetChildren = document.querySelector(target).firstChild;
-      const mr = getComputedStyle(targetChildren).marginRight;
-      const finishMr = +mr.substring(0, mr.length - 2) + scroll;
-      targetChildren.style.marginRight = `${finishMr}px`;
-      document.body.style.overflow = 'hidden';
-      document.body.style.marginRight = `${scroll}px`;
+      let scrollHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.body.clientHeight, document.documentElement.clientHeight);
+
+      if (scrollHeight > document.documentElement.clientHeight) {
+        const targetChildren = document.querySelector(target).firstChild;
+        const mr = getComputedStyle(targetChildren).marginRight;
+        const finishMr = +mr.substring(0, mr.length - 2) + scroll;
+        targetChildren.style.marginRight = `${finishMr}px`;
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
+      }
     });
   }
 
@@ -1941,9 +1945,14 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
       for (let i = 0; i < this.length; i++) {
         const target = this[i].getAttribute('data-target');
         const targetChildren = document.querySelector(target).firstChild;
-        const mr = getComputedStyle(targetChildren).marginRight;
-        const finishMr = +mr.substring(0, mr.length - 2) - scroll;
-        targetChildren.style.marginRight = `${finishMr}px`;
+
+        if (targetChildren.style.marginRight) {
+          const mr = getComputedStyle(targetChildren).marginRight;
+          const finishMr = +mr.substring(0, mr.length - 2) - scroll;
+          targetChildren.style.marginRight = `${finishMr}px`;
+        }
+
+        ;
       }
 
       document.body.style.marginRight = `0px`;
@@ -1957,9 +1966,14 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
       for (let i = 0; i < this.length; i++) {
         const target = this[i].getAttribute('data-target');
         const targetChildren = document.querySelector(target).firstChild;
-        const mr = getComputedStyle(targetChildren).marginRight;
-        const finishMr = +mr.substring(0, mr.length - 2) - scroll;
-        targetChildren.style.marginRight = `${finishMr}px`;
+
+        if (targetChildren.style.marginRight) {
+          const mr = getComputedStyle(targetChildren).marginRight;
+          const finishMr = +mr.substring(0, mr.length - 2) - scroll;
+          targetChildren.style.marginRight = `${finishMr}px`;
+        }
+
+        ;
       }
 
       document.body.style.marginRight = `0px`;
--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -24,12 +24,21 @@ $.prototype.modal = function() {
         $(this[i]).click((e) => {
             e.preventDefault();
             $(target).fadeIn(500);
-            const targetChildren = document.querySelector(target).firstChild;
-            const mr = getComputedStyle(targetChildren).marginRight;
-            const finishMr = +(mr.substring(0, mr.length - 2)) + scroll;
-            targetChildren.style.marginRight = `${finishMr}px`;
-            document.body.style.overflow = 'hidden';
-            document.body.style.marginRight = `${scroll}px`;
+
+            let scrollHeight = Math.max(
+                    document.body.scrollHeight, document.documentElement.scrollHeight,
+                    document.body.offsetHeight, document.documentElement.offsetHeight,
+                    document.body.clientHeight, document.documentElement.clientHeight
+                );
+
+            if (scrollHeight > document.documentElement.clientHeight) {
+                const targetChildren = document.querySelector(target).firstChild;
+                const mr = getComputedStyle(targetChildren).marginRight;
+                const finishMr = +(mr.substring(0, mr.length - 2)) + scroll;
+                targetChildren.style.marginRight = `${finishMr}px`;
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
+            }
         });
     }
 
@@ -38,14 +47,19 @@ $.prototype.modal = function() {
         $(elem).click(() => {
 
             $('.modal').fadeOut(500);
+
             for (let i = 0; i < this.length; i++) {
                 const target = this[i].getAttribute('data-target');
                 const targetChildren = document.querySelector(target).firstChild;
-                const mr = getComputedStyle(targetChildren).marginRight;
-                const finishMr = +(mr.substring(0, mr.length - 2)) - scroll;
-                targetChildren.style.marginRight = `${finishMr}px`;
+                
+                if (targetChildren.style.marginRight) {
+                    const mr = getComputedStyle(targetChildren).marginRight;
+                    const finishMr = +(mr.substring(0, mr.length - 2)) - scroll;
+                    targetChildren.style.marginRight = `${finishMr}px`;
+                };
+                
             }
-            
+                        
             document.body.style.marginRight = `0px`;
             document.body.style.overflow = '';
             
@@ -59,9 +73,12 @@ $.prototype.modal = function() {
             for (let i = 0; i < this.length; i++) {
                 const target = this[i].getAttribute('data-target');
                 const targetChildren = document.querySelector(target).firstChild;
-                const mr = getComputedStyle(targetChildren).marginRight;
-                const finishMr = +(mr.substring(0, mr.length - 2)) - scroll;
-                targetChildren.style.marginRight = `${finishMr}px`;
+
+                if (targetChildren.style.marginRight) {
+                    const mr = getComputedStyle(targetChildren).marginRight;
+                    const finishMr = +(mr.substring(0, mr.length - 2)) - scroll;
+                    targetChildren.style.marginRight = `${finishMr}px`;
+                };
             }
             document.body.style.marginRight = `0px`;
             document.body.style.overflow = '';
